ff0406a16b42c7150073ea40c520467e45fafad9
Dani <dany12rp13@gmail.com>
2020-11-16 21:45:33 -0300

    Pila

--- Prueba/Prueba/settings.py
+++ Prueba/Prueba/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku 
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,4 +128,3 @@ REST_FRAMEWORK = {
     ]
 }
 
-django_heroku.settings(locals())
